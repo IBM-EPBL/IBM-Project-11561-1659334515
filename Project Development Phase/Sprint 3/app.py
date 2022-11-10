e1460de205bd7bfa43b20c8b07ee22893af6a8b2
AK29112001 <113778960+AK29112001@users.noreply.github.com>
2022-11-10 06:46:57 +0530

    Update app.py

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -18,27 +18,20 @@ model = load_model("digit_classifier.h5")
 def index():
     return render_template('recognise.html')
 
-
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
     if request.method == "POST":
         f = request.files["image"]
         filepath = secure_filename(f.filename)
         f.save(os.path.join(app.config['UPLOAD_FOLDER'], filepath))
-
-        upload_img = os.path.join(FOLDER, filepath)
-        img = Image.open(upload_img).convert("L") 
+        uploading_img = os.path.join(FOLDER, filepath)
+        img = Image.open(uploading_img).convert("L") 
         img = img.resize((28, 28))
-
         im2arr = np.array(img)
         im2arr = im2arr.reshape(1, 28, 28, 1)
-
-        pred = model.predict(im2arr)
-
-        num = np.argmax(pred, axis=1)
-
+        predict = model.predict(im2arr)
+        num = np.argmax(predict, axis=1)
         return render_template('predict.html', num=str(num[0]))
-
 
 if __name__ == '__main__':
     app.run(debug=True, threaded=False)
