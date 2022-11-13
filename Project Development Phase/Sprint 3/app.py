92e9a157d131ba035ffaee7c22f8e2914ee1cf81
AK29112001 <113778960+AK29112001@users.noreply.github.com>
2022-11-13 18:26:34 +0530

    Update app.py

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -1,37 +1,36 @@
-import numpy as np
 import os
+import numpy as np
+from flask import Flask, render_template, request
+import tensorflow as tf
 from PIL import Image
-from flask import Flask, request, render_template, url_for
-from werkzeug.utils import secure_filename, redirect
-from gevent.pywsgi import WSGIServer
-from keras.models import load_model
-from keras.preprocessing import image
-from flask import send_from_directory
+from werkzeug.utils import secure_filename
 
-FOLDER = 'C:\Users\AKSHAYA\Desktop\ImageFiles'
+UPLOAD_FOLDER = 'C:\Users\AKSHAYA\Pictures\static\images'
+
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = FOLDER
-
-model = load_model("digit_classifier.h5")
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def index():
     return render_template('recognise.html')
 
-@app.route('/predict', methods=['GET', 'POST'])
-def upload():
-    if request.method == "POST":
-        f = request.files["image"]
-        filepath = secure_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_FOLDER'], filepath))
-        uploading_img = os.path.join(FOLDER, filepath)
-        img = Image.open(uploading_img).convert("L") 
-        img = img.resize((28, 28))
-        im2arr = np.array(img)
-        im2arr = im2arr.reshape(1, 28, 28, 1)
-        predict = model.predict(im2arr)
-        num = np.argmax(predict, axis=1)
-        return render_template('predict.html', num=str(num[0]))
 
-if __name__ == '__main__':
-    app.run(debug=True, threaded=False)
+model = tf.keras.models.load_model("digit_classifier.h5")
+
+@app.route('/predict', methods = ['GET','POST'])
+def upload_image_file():
+    if request.method == 'POST':
+        imagefile = request.files['image']
+        filename = secure_filename(imagefile.filename)
+        imagefile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        path_img = os.path.join(UPLOAD_FOLDER, filename)
+        img = Image.open(path_img).convert("L")
+        img = img.resize((28,28))
+        im2arr = np.array(img)
+        im2arr = im2arr.reshape(1,28,28,1)
+        y_pred = model.predict(im2arr)
+        
+        return render_template('predict.html', num = str(y_pred))
+
+if __name__ == '__main__' :
+    app.run(host='0.0.0.0', port=8000, debug=True)
