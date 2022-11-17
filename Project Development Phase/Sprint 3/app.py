dfbbdba7dfcb51347838f203568edf8bc15cbcce
AK29112001 <113778960+AK29112001@users.noreply.github.com>
2022-11-17 10:34:26 +0530

    Update app.py

--- Project Development Phase/Sprint 3/app.py
+++ Project Development Phase/Sprint 3/app.py
@@ -1,30 +1,26 @@
 import os
-import numpy as np
+
 from flask import Flask, render_template, request
-import tensorflow as tf
 from PIL import Image
 from werkzeug.utils import secure_filename
+import numpy as np
+import tensorflow as tf
 
-UPLOAD_FOLDER = 'C:\Users\AKSHAYA\Pictures\static\images'
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
+
+@app.route('/', methods=['GET'])
 def index():
     return render_template('recognise.html')
 
 
 model = tf.keras.models.load_model("digit_classifier.h5")
 
-@app.route('/predict', methods = ['GET','POST'])
-def upload_image_file():
+@app.route('/predict', methods = ['POST'])
+def predict():
     if request.method == 'POST':
-        imagefile = request.files['image']
-        filename = secure_filename(imagefile.filename)
-        imagefile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        path_img = os.path.join(UPLOAD_FOLDER, filename)
-        img = Image.open(path_img).convert("L")
+        img = Image.open(request.files['file']).convert("L")
         img = img.resize((28,28))
         im2arr = np.array(img)
         im2arr = im2arr.reshape(1,28,28,1)
@@ -33,4 +29,4 @@ def upload_image_file():
         return render_template('predict.html', num = str(y_pred))
 
 if __name__ == '__main__' :
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
